09d1d043f4501b8717cb79066becbd5ba3a0368e
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-03 09:41:59 +0100

    Updated README, added main.py and updated Lockbox source code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,49 @@
 #!/usr/bin/python3
-"""method that determines if all the boxes can be opened"""
+"""Method that determines if all the boxes can be opened.
+
+Args:
+    boxes (list): A list of lists, where each inner list
+    represents a box and its contents are the keys to other boxes.
+
+Returns:
+    bool: True if all boxes can be opened, False otherwise.
+
+Example:
+    >>> canUnlockAll([[1], [2], [3], [4]])
+    True
+    >>> canUnlockAll([[1, 4], [2], [3], [4]])
+    False
+    >>> canUnlockAll([[1, 3], [2], [0, 4], [4]])
+    True
+    >>> canUnlockAll([[]])
+    False
+    >>> canUnlockAll([])
+    False
+
+"""
 
 
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
 
+    The function takes a list of boxes as input,
+    where each box is a list of keys to other boxes.
+
+    It returns True if all boxes can be opened,
+    False otherwise.
+    """
     if (type(boxes) is not list):
         return False
 
-    if (len(boxes) == 0):
+    if len(boxes) == 0:
         return False
 
     keys = [0]
     for i in keys:
-        for j in boxes[i]:
-            if j not in keys and j != i and j < len(boxes) and j != 0:
-                keys.append(j)
+        for k in boxes[i]:
+            if k not in keys and k != i and k < len(boxes) and k != 0:
+                keys.append(k)
     if len(keys) == len(boxes):
         return True
     else:
